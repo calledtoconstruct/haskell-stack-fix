5c9ba21b15cd9c966038fcc292133bd8e40b023d
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-08-20 21:09:53 +0100

    Added lorri configuration and nix-shell

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "stack-fix";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [ base ];
+        homepage = "https://github.com/razvan-panda/haskell-stack-fix";
+        description = "Console program used to fix Stack build errors automatically";
+        license = stdenv.lib.licenses.agpl3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
